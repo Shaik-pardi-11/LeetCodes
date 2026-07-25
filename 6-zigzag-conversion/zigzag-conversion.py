67ebdb9927b7e1aed730ad6d1f7ef9ff694c9aae
Pardeen_SM <mdpardeen2006@gmail.com>
2026-07-25 11:02:30 +0530

    Time: 8 ms (69.92%) | Memory: 19.4 MB (46.57%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        curRow, step = 0, -1
+
+        for ch in s:
+            rows[curRow] += ch
+            if curRow == 0 or curRow == numRows - 1:
+                step *= -1
+            curRow += step
+
+        return "".join(rows)
+
+
+sol = Solution()
+print(sol.convert("PAYPALISHIRING", 3))  
+print(sol.convert("PAYPALISHIRING", 4))  
+print(sol.convert("A", 1))               
